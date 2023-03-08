e3c985070874819139104de08f78b3d9b022d8dd
aakj2010 <aakj2010@gmail.com>
2023-03-08 13:39:17 +0530

    mailview header added

--- src/components/pages/Sent.jsx
+++ src/components/pages/Sent.jsx
@@ -2,7 +2,12 @@ import React, { useContext } from 'react'
 import './Emailview.css'
 import StarOutlineOutlinedIcon from '@mui/icons-material/StarOutlineOutlined';
 import CheckBoxOutlineBlankOutlinedIcon from '@mui/icons-material/CheckBoxOutlineBlankOutlined';
-import LabelImportantOutlinedIcon from '@mui/icons-material/LabelImportantOutlined';
+import RefreshRoundedIcon from '@mui/icons-material/RefreshRounded';
+import MoreVertRoundedIcon from '@mui/icons-material/MoreVertRounded';
+import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+import ChevronLeftRoundedIcon from '@mui/icons-material/ChevronLeftRounded';
+import KeyboardRoundedIcon from '@mui/icons-material/KeyboardRounded';
+import ArrowDropDownRoundedIcon from '@mui/icons-material/ArrowDropDownRounded';
 import { SentData } from '../data/Primary'
 import ToggleContext from '../Context/ToggleContext'
 import { MdLabelImportantOutline } from 'react-icons/md';
@@ -28,6 +33,22 @@ function Sent() {
     return (
         <div className='tabledata mailview'>
         <MetaData title={'Sent'} />
+        <div className='inbox-head'>
+        <div className='head-icons'>
+          <button className="tdbtn"><CheckBoxOutlineBlankOutlinedIcon fontSize='small' /></button>
+          <button className="tdbtn"><RefreshRoundedIcon fontSize='small' /></button>
+          <button className="tdbtn"><MoreVertRoundedIcon fontSize='small' /></button>
+        </div>
+        <div className='head-icons'>
+          <div className='pagination'><p>1-50 of 2681</p></div>
+          <div className='head-icons'>
+          <button className="tdbtn"><ChevronLeftRoundedIcon fontSize='small' /></button>
+          <button className="tdbtn"><ChevronRightRoundedIcon fontSize='small' /></button>
+          <button className="tdbtn"><KeyboardRoundedIcon  fontSize='small' /></button>
+          <button className="arrow-btn"><ArrowDropDownRoundedIcon fontSize='medium' /></button>
+          </div>
+        </div>
+      </div>
         <table>
           <tbody className='tbody' >
             {
--- src/components/pages/Starred.jsx
+++ src/components/pages/Starred.jsx
@@ -3,7 +3,12 @@ import './Emailview.css'
 import { yellow } from '@mui/material/colors';
 import StarIcon from '@mui/icons-material/Star';
 import CheckBoxOutlineBlankOutlinedIcon from '@mui/icons-material/CheckBoxOutlineBlankOutlined';
-import LabelImportantOutlinedIcon from '@mui/icons-material/LabelImportantOutlined';
+import RefreshRoundedIcon from '@mui/icons-material/RefreshRounded';
+import MoreVertRoundedIcon from '@mui/icons-material/MoreVertRounded';
+import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+import ChevronLeftRoundedIcon from '@mui/icons-material/ChevronLeftRounded';
+import KeyboardRoundedIcon from '@mui/icons-material/KeyboardRounded';
+import ArrowDropDownRoundedIcon from '@mui/icons-material/ArrowDropDownRounded';
 import { StarredData} from '../data/Primary'
 import ToggleContext from '../Context/ToggleContext'
 import { MdLabelImportantOutline} from 'react-icons/md';
@@ -31,6 +36,22 @@ function Starred() {
   return (
     <div className='tabledata mailview'>
       <MetaData title={'Starred'} />
+      <div className='inbox-head'>
+        <div className='head-icons'>
+          <button className="tdbtn"><CheckBoxOutlineBlankOutlinedIcon fontSize='small' /></button>
+          <button className="tdbtn"><RefreshRoundedIcon fontSize='small' /></button>
+          <button className="tdbtn"><MoreVertRoundedIcon fontSize='small' /></button>
+        </div>
+        <div className='head-icons'>
+          <div className='pagination'><p>1-50 of 2681</p></div>
+          <div className='head-icons'>
+          <button className="tdbtn"><ChevronLeftRoundedIcon fontSize='small' /></button>
+          <button className="tdbtn"><ChevronRightRoundedIcon fontSize='small' /></button>
+          <button className="tdbtn"><KeyboardRoundedIcon  fontSize='small' /></button>
+          <button className="arrow-btn"><ArrowDropDownRoundedIcon fontSize='medium' /></button>
+          </div>
+        </div>
+      </div>
       <table>
         <tbody className='tbody' >
           {
